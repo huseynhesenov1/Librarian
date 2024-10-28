9a7ff9f2af4b3e3d578c31999e784fd9e4a3287e
huseyn-dev <huseyn.developer.15@gmail.com>
2024-10-29 00:57:20 +0400

    first commit

---
+++ Models/LibrarianService.cs
@@ -0,0 +1,89 @@
+﻿using LibraryMenegmentSistyem.Interface;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LibraryMenegmentSistyem.Models;
+
+public class LibrarianService : ILibrarianService
+{
+    private Librarian[] _librarians;
+    public LibrarianService(Librarian[] librarians)
+    {
+        _librarians = librarians;
+    }
+    public void CreateLibrarian(Librarian librarian)
+    {
+        Librarian[] newLibrarian = new Librarian[_librarians.Length + 1];
+        for (int i = 0; i < _librarians.Length; i++)
+        {
+            newLibrarian[i] = _librarians[i];
+        }
+        newLibrarian[_librarians.Length] = librarian;
+        _librarians = newLibrarian;
+
+    }
+
+    public void DeleteLibrarian(int id, bool isSoftDelete)
+    {
+        var librarian = GetLibrarianById(id);
+        if (librarian != null)
+        {
+            if (isSoftDelete)
+            {
+                
+            }
+            else
+            {
+                int index = Array.IndexOf(_librarians, librarian);
+                if (index >= 0)
+                {
+
+                    Librarian[] newLibrarians = new Librarian[_librarians.Length - 1];
+
+                    for (int i = 0; i < index; i++)
+                    {
+                        newLibrarians[i] = _librarians[i];
+                    }
+
+                    for (int i = index + 1; i < _librarians.Length; i++)
+                    {
+                        newLibrarians[i - 1] = _librarians[i];
+                    }
+
+                    _librarians = newLibrarians;
+                }
+            }
+        }
+        else
+        {
+            Console.WriteLine($"Student with Id {id} not found.");
+        }
+    
+}
+
+    
+
+    public Librarian[] GetAllLibrarians()
+    {
+
+        return _librarians;
+
+    }
+
+    public Librarian GetLibrarianById(int id)     
+    {
+        for (int i = 0; i < _librarians.Length; i++)
+        {
+            if (_librarians[i].Id == id)
+            {
+                return _librarians[i];
+            }
+        }
+        return default;
+    }
+
+   
+}
---
+++ Program.cs
@@ -0,0 +1,113 @@
+﻿using LibraryMenegmentSistyem.Interface;
+using LibraryMenegmentSistyem.Models;
+
+namespace LibraryMenegmentSistyem
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+
+            // indexer ucundu
+            //LibraryCatalog.Books();
+
+
+
+
+
+            Librarian librarian1 = new Librarian()
+            {
+                Id = 1,
+                Name = "Elekber",
+            };
+            Librarian librarian2 = new Librarian()
+            {
+                Id = 2,
+                Name = "Murad",
+            };
+            Librarian librarian3 = new Librarian()
+            {
+                Id = 3,
+                Name = "Zamin",
+            };
+            Librarian librarian4 = new Librarian()
+            {
+                Id = 4,
+                Name = "Ruslan",
+            };
+
+            Librarian[] librarians = { librarian1, librarian2, librarian3, librarian4 };
+
+            ILibrarianService librarianService = new LibrarianService(librarians);
+
+
+
+
+
+
+
+            #region GetAllLibrarians
+            //Librarian[] LibrarianAll = librarianService.GetAllLibrarians();
+            //foreach (var lb in LibrarianAll)
+            //{
+            //    Console.WriteLine($"Adi:{lb.Name} \nID:{lb.Id}");
+            //}
+
+            #endregion
+
+            #region GetLibrarianById
+            //Console.WriteLine("Axtarmaq istediyiniz kitabxanacinin ID-sini daxil edin");
+            //int searchId = int.Parse(Console.ReadLine());
+            //Librarian librarianObj = librarianService.GetLibrarianById(searchId);
+            //Console.WriteLine($"Id:{librarianObj.Id} \nAdi:{librarianObj.Name} ");
+
+            #endregion
+
+
+
+
+            #region DeleteLibrarian
+            //Console.WriteLine("Silmek istedyiniz elementin ID-sini yazin");
+            //int removeID = int.Parse(Console.ReadLine());
+            //librarianService.DeleteLibrarian(removeID, false);
+            //Librarian[] librarianAll = librarianService.GetAllLibrarians();
+            //foreach (var st in librarianAll)
+            //{
+            //    Console.WriteLine($"Adi:{st.Name} \nSoyadi:{st.Id}");
+            //}
+            #endregion
+
+
+
+            #region CreateLibrarian
+            Librarian newLibrarian = new Librarian
+            {
+                Id = default,
+                Name = null,
+
+            };
+            Console.Write("Elave etmek istediyin telebenin ID-sini yaz: ");
+            newLibrarian.Id = int.Parse(Console.ReadLine());
+            Console.Write("Elave etmek istediyin telebenin adini yaz: ");
+            newLibrarian.Name = Console.ReadLine();
+           
+            Console.WriteLine("Yeni telebe elave edildi");
+            librarianService.CreateLibrarian(newLibrarian);
+            Librarian[] updatedLibrarian = librarianService.GetAllLibrarians();
+            foreach (var st in updatedLibrarian)
+            {
+                Console.WriteLine($"Adi:{st.Name} \nId:{st.Id}");
+            }
+            #endregion
+
+
+
+
+
+
+
+
+
+        }
+    }
+}
